1edaa95023fce596246dff4190da37bec399422e
Sander Ronde <awsdfgvhbjn@gmail.com>
2016-11-09 21:16:37 +0100

    set up basic static server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,18 @@
+"use strict";
+var http = require('http');
+var path = require('path');
+var express = require('express');
+var Router = require('router');
+var finalHandler = require('finalhandler');
+var PORT = 1234;
+var router = new Router();
+router.use(express.static(path.join(__dirname, '../app'), {
+    maxAge: 60 * 60 * 24
+}));
+function reqHandler(req, res) {
+    router(req, res, finalHandler(req, res));
+}
+http.createServer(reqHandler).listen(PORT, function () {
+    console.log("HTTP server listening on port " + PORT);
+});
 //# sourceMappingURL=server.js.map
--- server/server.ts
+++ server/server.ts
@@ -1,1 +1,23 @@
 /// <reference path="../typings/tsd.d.ts" />
+
+import * as http from 'http';
+import * as path from 'path';
+import * as express from 'express';
+
+const Router = require('router');
+const finalHandler = require('finalhandler');
+
+const PORT = 1234;
+
+const router = new Router();
+router.use(express.static(path.join(__dirname, '../app'), {
+	maxAge: 60 * 60 * 24
+}));
+
+function reqHandler(req: http.IncomingMessage, res: http.ServerResponse) {
+	router(req, res, finalHandler(req, res));
+}
+
+http.createServer(reqHandler).listen(PORT, () => {
+	console.log(`HTTP server listening on port ${PORT}`);
+});
